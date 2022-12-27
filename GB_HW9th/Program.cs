1c1f4bd8118f4fa31f35c8b151619632424ec764
Viktoriia Nekrasova <vvinebrand@gmail.com>
2022-12-28 02:01:55 +0300

    Первая задача

--- GB_HW9th/Program.cs
+++ GB_HW9th/Program.cs
@@ -6,7 +6,7 @@ Boolean begin = true;
 
 while (begin)
 {
-    Console.WriteLine("\nЗадача 1. \nЗадача 2.\nЗадача 3.\nВведите 0 для выхода\n");
+    Console.WriteLine("\nЗадача 1. Вывод всех натуральных чисел в промежутке от N до 1 \nЗадача 2.\nЗадача 3.\nВведите 0 для выхода\n");
     Console.Write("Введите номер задачи: ");
     programm = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
@@ -15,6 +15,12 @@ while (begin)
     {
         case 1:
 
+            Console.Write("Введите натуральное число больше 1: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            NumberCounter(number);
+            Console.WriteLine();
 
             break;
 
@@ -31,4 +37,13 @@ while (begin)
             begin = false;
             break;
     }
+}
+
+
+void NumberCounter(int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}",number);
+    NumberCounter(number - 1);
 }
