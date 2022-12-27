0df33a40b68f5eca08b33bd4bb1ac4210f757c7f
Viktoriia Nekrasova <vvinebrand@gmail.com>
2022-12-28 01:55:19 +0300

    Добавьте файлы проекта.

---
+++ GB_HW9th/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+int programm;
+Boolean begin = true;
+
+while (begin)
+{
+    Console.WriteLine("\nЗадача 1. \nЗадача 2.\nЗадача 3.\nВведите 0 для выхода\n");
+    Console.Write("Введите номер задачи: ");
+    programm = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    switch (programm)
+    {
+        case 1:
+
+
+            break;
+
+        case 2:
+
+
+            break;
+
+        case 3:
+
+
+            break;
+        default:
+            begin = false;
+            break;
+    }
+}
